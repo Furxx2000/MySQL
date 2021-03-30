cd6d26e01a6981bb41c89e8cfcbf0a9c84367ac0
furxx2000 <furxx2000@gmail.com>
2021-03-30 12:06:16 +0800

    origin

---
+++ test/a20210330.php
@@ -0,0 +1,20 @@
+
+<?php
+$a = 22;
+$b = '24';
+$c = 'str';
+
+print $a + $b;
+print '<br>';
+
+echo $a * $b;
+echo '<br>';
+
+$a = 'Victor';
+$b = 25;
+$c = 'programmer';
+
+echo "${a} is a {$b} years old $c";
+echo '<br>';
+echo 'Hello $a';
+?>
